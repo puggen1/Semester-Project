27abe061b83da17d77ad9da90481a2bce2c3ff61
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2022-12-07 10:32:18 +0100

    test carousel backend is fixed, now only animation is needed

--- testcarousel.js
+++ testcarousel.js
@@ -40,30 +40,31 @@ function createCarousel(media, placement) {
       "col-md-2",
       "flex-lg-row"
     );
-    smallPart.innerHTML = `<div class="carouselButtons d-md-flex justify-content-md-center col-1 col-md-12 col-lg-1">
+    let smallPartHtml = "";
+    smallPartHtml = `<div class="carouselButtons d-md-flex justify-content-md-center col-1 col-md-12 col-lg-1">
     <button class="btn" type="button" data-bs-target="#carouselBigImage" data-bs-slide="prev">
         <span class="bi bi-chevron-left d-md-none d-lg-block" aria-hidden="true"></span>
         <span class="bi bi-chevron-up d-none d-md-block d-lg-none" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </button>
 </div>`;
-    smallPart.innerHTML += `<div class="col-10 d-flex flex-md-column flex-lg-row justify-content-around">`;
+    smallPartHtml += `<div class="col-10 d-flex flex-md-column flex-lg-row justify-content-around">`;
     let x = 1;
     for (let i = 0; i < media.length; i++) {
       if (i > 2) {
-        smallPart.innerHTML += `
-            <button type="button" data-bs-target="#carouselBigImage" data-bs-slide-to="${i}" class="active d-none btn btn-link col-3 col-md-12 p-md-0 my-md-3" aria-current="true" aria-label="Slide ${x}">
+        smallPartHtml += `
+            <button type="button" data-bs-target="#carouselBigImage" data-bs-slide-to="${i}" class="active d-none btn btn-link col-3 col-md-12 p-0 my-md-3" aria-current="true" aria-label="Slide ${x}">
             <img src="${media[i]}" class="d-block w-100" alt="listing Image">
             </button>`;
       } else {
-        smallPart.innerHTML += `
-        <button type="button" data-bs-target="#carouselBigImage" data-bs-slide-to="${i}" class="active btn btn-link col-3 col-md-12 p-md-0 my-md-3" aria-current="true" aria-label="Slide ${x}">
+        smallPartHtml += `
+        <button type="button" data-bs-target="#carouselBigImage" data-bs-slide-to="${i}" class="active btn btn-link col-3 col-md-12 p-0 my-md-3" aria-current="true" aria-label="Slide ${x}">
         <img src="${media[i]}" class="d-block w-100" alt="listing Image">
         </button>`;
       }
       x++;
     }
-    smallPart.innerHTML += `</div>
+    smallPartHtml += `</div>
     <div class="carouselButtons d-md-flex justify-content-md-center col-1 col-md-12 col-lg-1">
     <button class="btn" type="button" data-bs-target="#carouselBigImage" data-bs-slide="next">
         <span class="bi bi-chevron-right d-md-none d-lg-block" aria-hidden="true"></span>
@@ -72,13 +73,15 @@ function createCarousel(media, placement) {
       </button>
 </div>
     `;
+    smallPart.innerHTML = smallPartHtml;
   } else {
     bigPart.innerHTML = `<img src="./assets/noImage.jpg" class="d-block w-100" alt="listing has no image">`;
   }
   carouselouter.insertAdjacentElement("beforeend", bigPart);
   carouselouter.insertAdjacentElement("beforeend", smallPart);
   placement.insertAdjacentElement("beforeend", carouselouter);
-  testButtons();
+  imgButtons();
+  arrowButtons();
 }
 let carouselPlacement = document.getElementById("carouselPlacement");
 let mediaList = [
@@ -86,38 +89,76 @@ let mediaList = [
   "https://picsum.photos/500/300?random=2",
   "https://picsum.photos/500/300?random=3",
   "https://picsum.photos/500/300?random=4",
+  "https://picsum.photos/500/300?random=5",
+  "https://picsum.photos/500/300?random=6",
 ];
 let numberOfSlides = mediaList.length;
 let currentSlide = 0;
 createCarousel(mediaList, carouselPlacement);
-function testButtons() {
+
+function imgButtons() {
   let buttons = document.querySelectorAll("#lowerMedia button.btn-link");
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
-      currentSlide = button.getAttribute("data-bs-slide-to");
-      buttons.forEach((button) => {
-        if (
-          button.getAttribute("data-bs-slide-to") == currentSlide ||
-          button.getAttribute("data-bs-slide-to") == currentSlide - 1 ||
-          button.getAttribute("data-bs-slide-to") == currentSlide + 1
-        ) {
-          console.log(button.getAttribute("data-bs-slide-to"), currentSlide);
-          button.classList.remove("d-none");
-        } else if (
-          button.getAttribute("data-bs-slide-to") == 0 &&
-          currentSlide == 0
-        ) {
-          console.log(
-            button.getAttribute("data-bs-slide-to"),
-            currentSlide,
-            numberOfSlides
-          );
-          buttons[numberOfSlides - 1].classList.remove("d-none");
-        } else {
-          console.log(button.getAttribute("data-bs-slide-to"), currentSlide);
-          button.classList.add("d-none");
+      currentSlide = Number(button.getAttribute("data-bs-slide-to"));
+      let left = currentSlide - 1;
+      let right = currentSlide + 1;
+
+      if (left < 0) {
+        left = currentSlide + 2;
+      }
+      if (right > numberOfSlides - 1) {
+        right = currentSlide - 1;
+        left = currentSlide - 2;
+      }
+
+      buttons[left].classList.remove("d-none");
+      buttons[right].classList.remove("d-none");
+      buttons[currentSlide].classList.remove("d-none");
+      for (let i = 0; i < buttons.length; i++) {
+        if (i !== currentSlide && i !== left && i !== right) {
+          buttons[i].classList.add("d-none");
         }
-      });
+      }
+    });
+  });
+}
+
+function arrowButtons() {
+  let buttons = document.querySelectorAll(".carouselButtons button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      let imageSlides = document.querySelectorAll(
+        "#lowerMedia button.btn-link"
+      );
+      if (button.getAttribute("data-bs-slide") === "next") {
+        currentSlide++;
+        if (currentSlide > numberOfSlides - 1) {
+          currentSlide = 0;
+        }
+      } else {
+        currentSlide--;
+        if (currentSlide < 0) {
+          currentSlide = numberOfSlides - 1;
+        }
+      }
+      let left = currentSlide - 1;
+      let right = currentSlide + 1;
+      if (left < 0) {
+        left = currentSlide + 2;
+      }
+      if (right > numberOfSlides - 1) {
+        right = currentSlide - 1;
+        left = currentSlide - 2;
+      }
+      imageSlides[left].classList.remove("d-none");
+      imageSlides[right].classList.remove("d-none");
+      imageSlides[currentSlide].classList.remove("d-none");
+      for (let i = 0; i < numberOfSlides; i++) {
+        if (i !== currentSlide && i !== left && i !== right) {
+          imageSlides[i].classList.add("d-none");
+        }
+      }
     });
   });
 }
