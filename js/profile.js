3159ede7d27049fb6f9739c4d7baa4645efc019b
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2022-12-09 19:42:56 +0100

    added more profile content

--- js/profile.js
+++ js/profile.js
@@ -8,25 +8,30 @@ let url = new URL(window.location.href);
 let user = url.searchParams.get("username");
 async function showProfile(){
 if(storageRetriever("isLoggedIn")){
+    let profileData;
     if(!user){
         let userToGet = storageRetriever("username");
-        let profileData = await apiCall(`profiles/${userToGet}?_listings=true&_seller=true&_bids=true`, "GET", null,storageRetriever("loginToken"));
+        profileData = await apiCall(`profiles/${userToGet}?_listings=true&_seller=true&_bids=true`, "GET", null,storageRetriever("loginToken"));
         let listings = await apiCall(`profiles/${userToGet}/listings?_bids=true&_seller=true`, "GET", null,storageRetriever("loginToken"));
-
+        token.innerText = profileData.credits;
         console.log(profileData);
         console.log(listings);
         profileListings(listings);
     }
     else{
-        let profileData = await apiCall(`profiles/${user}?_listings=true`, "GET", null,storageRetriever("loginToken"));
+        profileData = await apiCall(`profiles/${user}?_listings=true`, "GET", null,storageRetriever("loginToken"));
+        document.querySelector("#userTokens").remove();
+        document.querySelector("#editPicture").remove();
         let listings = await apiCall(`profiles/${user}/listings?_bids=true&_seller=true`, "GET", null,storageRetriever("loginToken"));
         console.log(profileData);
         profileListings(listings);
+      
 
     }
+    username.innerText = profileData.name;
+    
 }
-username.innerText = storageRetriever("username");
-token.innerText = storageRetriever("token");
+
 }
 
 
